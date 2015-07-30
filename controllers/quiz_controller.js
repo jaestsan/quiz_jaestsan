e1cbcfcc50926901aa1f91ff6dc3b18db0743c29
javier <javi.esteban@gmail.com>
2015-07-30 14:48:55 +0200

    autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,45 +1,34 @@
 var models = require('../models/models.js');
 
-/*exports.question = function(req, res) {
-        models.Quiz.findAll().success(function(quiz) {
-                res.render('quizes/question', {pregunta: quiz[0].pregunta});
-        })
-};
-
-exports.answer = function(req, res) {
-        models.Quiz.findAll().then(function(quiz) {
-                if (req.query.respuesta === quiz[0].respuesta) {
-                        res.render('quizes/answer', {respuesta: 'Correcto'});
-                }else{
-                        res.render('quizes/answer', {respuesta: 'Incorrecto'});
-                }
-        })
-};*/
-
-
-//GET /quizes/:id
-exports.show = function(req, res) {
-	 models.Quiz.find(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', {quiz: quiz});
-         })
-};
-
-//GET /quizes/:id/answer
-exports.answer = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz) {
-        	if (req.query.respuesta === quiz.respuesta ) {
-			res.render('quizes/answer',
-				   {quiz: quiz, respuesta: 'Correcto'});
-        	}else{
-                	res.render('quizes/answer',
-				   {quiz: quiz, respuesta: 'Incorrecto'});
-               	}
-	})
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+	 function(quiz){
+	   if (quiz) {
+	       req.quiz = quiz;
+    	       next()
+	   }else { next (new Error('No existe quizId=' + quizId)); }
+	  }
+	).catch(function(error) { next(error);});
 };
 
 //GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(function(quizes) {
-	     res.render('quizes/index.ejs', {quizes: quizes});
-	})
+        models.Quiz.findAll().then(function(quizes) {
+             res.render('quizes/index.ejs', {quizes: quizes});
+        })
+};
+
+
+//GET /quizes/:id
+exports.show = function(req, res) {
+   res.render('quizes/show', {quiz: req.quiz});
+};
+
+//GET /quizes/:id/answer
+exports.answer = function(req, res) {
+	var resultado = 'Incorrecto';
+       	if (req.query.respuesta === req.quiz.respuesta ) {
+	   resultado = 'Correcto';
+        }
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
